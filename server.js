f8f3dc2cb7b899a00430e46c98b74eb1a93d4524
Truong Tri Tin <eriegin@nexondv.com>
2023-01-31 16:57:01 +0700

    add user route

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const app = express();
+const createError = require("http-errors");
+const UserRoute = require("./Routes/User.route");
 
 require("dotenv").config();
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res, next) => {
   res.send("home page");
+});
+
+app.use("/user", UserRoute);
+
+app.use((req, res, next) => {
+  // const error = new Error("Not Found");
+  // error.status = 500;
+  // next(error);
+  next(createError.NotFound("This route does not exist"));
+});
+
+app.use((err, req, res, next) => {
+  res.json({ status: err.status || 500, message: err.message });
 });
 
 app.listen(PORT, () => {
