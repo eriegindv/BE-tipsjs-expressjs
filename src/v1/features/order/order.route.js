62544cb52b89e27ae51ea6f7d0282748155c5c32
Truong Tri Tin <eriegin@nexondv.com>
2023-03-06 10:51:26 +0700

    change route

--- src/v1/features/order/order.route.js
+++ src/v1/features/order/order.route.js
@@ -3,7 +3,7 @@ const { order, cancelExpiration } = require("./order.controller");
 
 const route = express.Router();
 
-route.get("/", order);
+route.get("/concurrence-order", order);
 route.post("/expiration-order", cancelExpiration);
 
 module.exports = route;
